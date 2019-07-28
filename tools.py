e5c4034fea6b8fec4be6cc9be7e53a01f342750b
angkatan24 <42346081+npmbots@users.noreply.github.com>
2019-07-28 19:05:59 +0700

    Update tools.py

--- tools.py
+++ tools.py
@@ -9,3 +9,15 @@ dgray = "\033[1;30m"
 ir = "\033[0;101m"
 reset = "\033[0m"
 
+print "==========================".format(r)
+print "| Author : Mr.Y0uKn0wM3  |".format(r)
+print "| Content : 4 Tools      |".format(r)
+print "| Updated at 2019-07-28  |".format(r)
+print "==========================".format(r)
+
+print ""
+
+print "==== M E N U ====".format(w)
+print ""
+print "1) SQL MAP".format(r, w)
+print "2"
