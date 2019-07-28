8f63ba3992c58e8f3c64e61920910445aa89fb01
angkatan24 <42346081+npmbots@users.noreply.github.com>
2019-07-28 18:52:09 +0700

    Create cctv.sh

---
+++ cctv.sh
@@ -0,0 +1,17 @@
+echo "Memulai.."
+sleep 0.8
+clear
+echo "=================="
+echo "|   Thanks to     |"
+echo "|   Mr.TamfanX    |"
+echo "|       &         |"
+echo "|    Mr.Uncle     |"
+echo "=================="
+sleep 2
+clear
+git clone https://github.com/MrTamfanX/hackcctv
+cd hackcctv
+clear
+echo "[+] MASUK KE HALAMAN HOME PAGE [+]"
+sleep 1
+python2 CctvX.py
