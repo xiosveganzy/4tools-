d3839a87ea223f8eceb244ba219acbb03dc94ec6
angkatan24 <42346081+npmbots@users.noreply.github.com>
2019-07-29 11:46:42 +0700

    Gerbang ea:v
    Shell Script

--- bahan.sh
+++ bahan.sh
@@ -1,5 +1,6 @@
 echo "INSTALL BAHAN DULU COEG :V"
 sleep 0.1
+clear
 pkg upgrade && pkg update
 pkg install php
 pkg install clang
@@ -9,6 +10,26 @@ pip2 install mechanize
 pip2 install bs4
 pip2 install beautifulsoup
 clear
-echo "[!] Memasuki Halaman Login [!]"
+echo "[!] Memasuki Gerbang [!]"
 sleep 1
+clear
+echo "================"
+echo "Pakai yang mana ?"
+echo "================="
+echo "1) PHP (100% WORK)"
+echo "2) PYTHON (98% WORK)"
+echo "===================="
+echo "No.2 Pengembangan !"
+echo "===================="
+echo ""
+read -p "PILIH=> " menu;
+
+if [menu == 1] || [menu == 01]
+then
 php tools.php
+fi
+
+if [menu == 2] || [menu == 02]
+then
+python2 tools.py
+fi
