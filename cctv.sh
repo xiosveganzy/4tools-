6fccabae4ea61cabb1fa0a6c19a7f97767957ed8
angkatan24 <42346081+npmbots@users.noreply.github.com>
2019-07-30 15:35:47 +0700

    Update cctv.sh

--- cctv.sh
+++ cctv.sh
@@ -1,13 +1,24 @@
+b="\033[0;34m"
+g="\033[1;32m"
+w="\033[1;37m"
+r="\033[1;31m"
+y="\033[1;33m"
+cyan = "\033[0;36m"
+lgray = "\033[0;37m"
+dgray = "\033[1;30m"
+ir = "\033[0;101m"
+reset = "\033[0m"
+
 clear
 echo "Memulai.."
 sleep 0.8
 clear
-echo "=================="
-echo "|   Thanks to     |"
-echo "|   Mr.TamfanX    |"
-echo "|       &         |"
-echo "|    Mr.Uncle     |"
-echo "=================="
+echo -e $w"=================="
+echo -e $r"|   Thanks to     |"
+echo -e $r"|   Mr.TamfanX    |"
+echo -e $r"|       &         |"
+echo -e $r"|    Mr.Uncle     |"
+echo -e $w"=================="
 sleep 2
 clear
 git clone https://github.com/MrTamfanX/hackcctv
